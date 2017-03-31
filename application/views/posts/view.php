de7ffa6bbfb70b4024a4776956ef2e0cfcacc3b2
Chris Wright <christwright21@gmail.com>
2017-03-31 15:10:32 -0400

    initial commit

---
+++ application/views/posts/view.php
@@ -0,0 +1,11 @@
+<h2><?php echo $post['title']; ?></h2>
+<small class="post-date">Posted on: <?php echo $post['created_at']; ?> </small><br>
+<div class="post-body">
+<?php echo $post['body']; ?>
+</div>
+
+<hr>
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+	<input type="submit" value="Delete" class="btn btn-danger">
+</form>
+<a href="posts/edit/<?php echo $post['id']; ?>">Edit</a>
